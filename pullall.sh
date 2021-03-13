b22aeaf6895b847086896e09bd2372295c61bf35
dolanseesu <schimomsch@gmail.com>
2021-03-14 00:29:34 +0100

    completed

--- pullall.sh
+++ pullall.sh
@@ -1,17 +1,23 @@
-#!/bin/bash
+#!/bin/sh
+
+# this script goes through all my git repos and executes a git pull
+
 
 # array with dirs
-
 dirs=(dolanseesu.github.io dotfiles markdown python studium)
 
 # ${dirs[*]} - list elements of array
 # ${!dirs[*]} - list indices of array
 
 for i in ${dirs[*]}; do
-    if [[ -d $i ]]; then
-        echo "cd into $i..."
-        cd ~/$i
+    if [ -d ${i} ]; then
+        echo "cd into ${i}..."
+        cd ~/${i} || exit 1
         git pull
-        cd
+        cd || exit 1
     fi
 done
+
+cd || exit 1
+echo -e "\nAll done."
+exit 0
