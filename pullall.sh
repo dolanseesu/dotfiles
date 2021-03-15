73e84f345f05b6ab16401dabdbb6353242f68bc2
dolanseesu <schimomsch@gmail.com>
2021-03-15 21:26:49 +0100

    unified code and bugfixes

--- pullall.sh
+++ pullall.sh
@@ -1,20 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # this script goes through all my git repos and executes a git pull
 
-
 # array with dirs
-dirs=(dolanseesu.github.io dotfiles markdown python studium)
+git_dirs=(dolanseesu.github.io dotfiles markdown python studium)
 
-# ${dirs[*]} - list elements of array
-# ${!dirs[*]} - list indices of array
+# ${dirs[@]} - list elements of array
+# ${!dirs[@]} - list indices of array
 
 cd ~/ || exit 1
 
-for i in ${dirs[*]}; do
-    if [ -d ${i} ]; then
-        echo "cd into ${i}..."
-        cd ~/${i} || exit 1
+for i in "${git_dirs[@]}"; do
+    if [ -d "$i" ]; then
+        echo -e "\ncd into ${i}..."
+        cd ~/"$i" || exit 1
         git pull
         cd || exit 1
     fi
