35f63fa5fe7b3ea309437bc963c7e40e51e01482
dolanseesu <schimomsch@gmail.com>
2021-03-16 15:31:38 +0100

    made script POSIX compliant

--- install.sh
+++ install.sh
@@ -1,34 +1,31 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
 
 # creates symbolic links for my dotfiles so all files can stay in ~/dotfiles
 # WARNING: this script will overwrite existing files that match the filenames from the dotfiles repo!
-# tested with bash, zsh and sh. other shells may not work
+# this script should be POSIX compliant.
 
 dotdir=~/dotfiles   # specify the FULL path to the repo here
 laptop=false
 
-# make sure to start in the home directory
-cd ~/ || exit 1
-
 echo "Configure dotfiles for the laptop? (y/n)"
-read usrcmd
+read -r usrcmd
 
-if [ "$usrcmd" = "y" ]; then
+if [ "$usrcmd" = "y" ] || [ "$usrcmd" = "Y" ]; then
     laptop=true
 fi
 
 # check if dotfiles repo exists
 if [ ! -d "$dotdir" ]; then
-    echo "${dotdir} not found, either clone the git repo or specift the correct path above."
-    echo "Exiting."
+    echo "${dotdir} not found, either clone the git repo or specift the correct path, exiting."
     exit 1
 fi
 
+# make sure to start in the home directory
+cd ~/ || exit 1
+
 # start creating symlinks
 # files in ~/
-files=(.bashrc .zshrc .profile .vimrc)
-
-for i in "${files[@]}"; do
+for i in .bashrc .zshrc .profile .vimrc; do
     if [ -f "$i" ]; then
         rm -f "$i"
     fi
@@ -36,17 +33,13 @@ for i in "${files[@]}"; do
 done
 
 # MOC
-# install moc first and  start it once to avoid errors!
 if [ "$laptop" = false ]; then
     if [ ! -d ~/.moc ]; then
         mkdir -p ~/.moc
     fi
-
     cd ~/.moc || exit 1
 
-    moc_files=(eqsets themes config equalizer)
-
-    for i in "${moc_files[@]}"; do
+    for i in eqsets themes config equalizer; do
         if [ -d "$i" ] || [ -f "$i" ]; then
             rm -rf "$i"
         fi
@@ -58,18 +51,18 @@ fi
 if [ ! -d ~/.config ]; then
     mkdir -p ~/.config
 fi
-
 cd ~/.config || exit 1
 
 if [ "$laptop" = true ]; then
-    config_dirs=(neofetch zathura)  # only create symlinks
-    copy_dirs=(alacritty awesome)   # copy these directories
-    
-    for i in "${copy_dirs[@]}"; do
+    for i in alacritty awesome; do
         if [ -d "$i" ] || [ -f "$i" ]; then
             rm -rf "$i"
         fi
-        cp -r "$dotdir"/"$i" .
+        if [ "$i" = "alacritty" ] || [ "$i" = "awesome" ]; then
+            cp -r "$dotdir"/"$i" .
+        else
+            ln -sf "$dotdir"/"$i" "$i"
+        fi
     done
     
     # put laptop dotfiles in place
@@ -78,25 +71,22 @@ if [ "$laptop" = true ]; then
     rm -f awesome/rc.lua
     mv awesome/rc_laptop.lua awesome/rc.lua
 else
-    config_dirs=(alacritty awesome neofetch zathura)
+    for i in alacritty awesome neofetch zathura; do
+        if [ -d "$i" ] || [ -f "$i" ]; then
+            rm -rf "$i"
+        fi
+        ln -sf "$dotdir"/"$i" "$i"
+    done
 fi
 
-for i in "${config_dirs[@]}"; do
-    if [ -d "$i" ] || [ -f "$i" ]; then
-        rm -rf "$i"
-    fi
-    ln -sf "$dotdir"/"$i" "$i"
-done
-
+# Neovim
 if [ ! -d nvim ]; then
     mkdir -p nvim
 fi
-
 cd nvim || exit 1
-
 ln -sf "$dotdir"/nvim/init.vim init.vim
 
+# finish
 cd ~/ || exit 1
 echo "All done, no errors."
-
 exit 0
